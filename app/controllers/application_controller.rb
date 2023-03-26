e996bd6f66cc61847e4e8ee443ce574edf63106a
David Morales <david@davidmles.com>
2023-03-26 14:15:42 +0200

    Final code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   def page1; end
 
   def page2; end
+
+  def page3
+    redirect_to page2_path, status: :see_other
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get 'page1', to: 'application#page1'
   get 'page2', to: 'application#page2'
+  post 'page3', to: 'application#page3'
 end
