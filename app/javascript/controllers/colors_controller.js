db4ffe3147e77f4c44000d42ad0da54aa10cd369
David Morales <david@davidmles.com>
2023-03-28 10:05:52 +0200

    Final code

--- app/javascript/controllers/colors_controller.js
+++ app/javascript/controllers/colors_controller.js
@@ -2,6 +2,12 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="colors"
 export default class extends Controller {
+  static targets = ['input', 'card'];
+
   connect() {
+  }
+
+  inputTargetConnected() {
+    this.cardTarget.style = 'background-color: red';
   }
 }
