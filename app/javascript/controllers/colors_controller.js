48ccb308974f7cca37886b46334a7db2ba21fa0c
David Morales <david@davidmles.com>
2023-03-28 10:18:16 +0200

    Final code

--- app/javascript/controllers/colors_controller.js
+++ app/javascript/controllers/colors_controller.js
@@ -3,6 +3,7 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="colors"
 export default class extends Controller {
   static targets = ['input', 'card'];
+  static classes = ['reset'];
 
   connect() {
   }
@@ -10,5 +11,11 @@ export default class extends Controller {
   colorizeCard() {
     this.cardTarget.classList.remove('bg-white');
     this.cardTarget.style = `background-color: ${this.inputTarget.value}`;
+  }
+
+  resetCard() {
+    this.cardTarget.style = '';
+    this.cardTarget.classList.remove('border-black');
+    this.cardTarget.classList.add(...this.resetClasses);
   }
 }
