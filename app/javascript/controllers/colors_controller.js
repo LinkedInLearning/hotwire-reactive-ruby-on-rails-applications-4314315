1b763b557a7e401ea5726ac5370411e6909ec7e9
David Morales <david@davidmles.com>
2023-03-28 08:03:48 +0200

    Final code

--- app/javascript/controllers/colors_controller.js
+++ app/javascript/controllers/colors_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
+// Connects to data-controller="colors"
 export default class extends Controller {
   connect() {
-    this.element.textContent = "Hello World!"
   }
 }
